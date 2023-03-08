a8e564dbea3fb79926e845814f9679722da4f7ab
ray.esro <ray.esro@gmail.com>
2023-03-07 22:00:54 -0600

    Codigos vistos en clase y sesoines virtuales

---
+++ codigo/MSTeamsDataExtractor/Program.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.TermStore;
+
+namespace MSTeamsDataExtractor
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var scopes = new[] { "User.Read", "Team.ReadBasic.All" , "EduRoster.ReadBasic" };
+
+      // Multi-tenant apps can use "common",
+      // single-tenant apps must use the tenant ID from the Azure portal
+      var tenantId = "caca9011-7b6a-44de-861f-095a2ca883b7";
+
+      // Value from app registration
+      var clientId = "465ea0d4-3510-4173-9ccd-e42e967ef20e";
+
+      // using Azure.Identity;
+      var options = new InteractiveBrowserCredentialOptions
+      {
+        TenantId = tenantId,
+        ClientId = clientId,
+        AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+        // MUST be http://localhost or http://localhost:PORT
+        // See https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/System-Browser-on-.Net-Core
+        RedirectUri = new Uri("http://localhost"),
+      };
+
+      // https://learn.microsoft.com/dotnet/api/azure.identity.interactivebrowsercredential
+      var interactiveCredential = new InteractiveBrowserCredential(options);
+
+      var graphClient = new GraphServiceClient(interactiveCredential, scopes);
+
+      var classes = await graphClient.Education.Classes
+        .Request()
+        .GetAsync();
+
+      foreach (var c in classes) {
+        Console.WriteLine($"Title: {c.DisplayName}, ID: {c.Id}");
+      }
+      //  var joinedTeams = await graphClient.Teams["752e349e-9a55-4991-8c55-89b2fc9a8bb5"]
+      //    .InstalledApps[""]
+      //    .Me.JoinedTeams
+      //    .Request()
+      //    .GetAsync();
+
+      //  // Output the assignment data
+      //  foreach (var team in joinedTeams)
+      //  {
+      //    Console.WriteLine($"Title: {team.DisplayName}, ID: {team.Id}");
+      //  }
+      //  var misTeams = joinedTeams.Where(x => x.Id == "752e349e-9a55-4991-8c55-89b2fc9a8bb5" || x.Id == "5ef8a413-32dc-44ec-b47d-39576207cbd3\r\n").ToList();
+      ////  var events = await graphClient.Groups["{group-id}"].Events
+      ////.Request()
+      ////.GetAsync();
+
+
+    }
+  }
+}
---
+++ codigo/Winapi/Gpo04/Entrega01/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Entrega01.rc
+//
+#define DLG_INICIOSESION                101
+#define IDD_DIALOG2                     103
+#define DLG_ALTAUSUARIO                 103
+#define IDC_BUTTON1                     1001
+#define BTN_INICIARSESION_PINICIOSESION 1002
+#define TXT_Usuario_PInicioSesion       1003
+#define TXT_CONTRASENA_PINICIOSESION    1004
+#define IDC_COMBO1                      1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
